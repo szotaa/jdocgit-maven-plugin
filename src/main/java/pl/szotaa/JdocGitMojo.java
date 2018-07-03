7c9831bf4d0636763feb07185cfc5b3f684dfdaf
szotaa <kubaszota97@gmail.com>
2018-07-03 14:50:30 +0200

    write documentation

--- src/main/java/pl/szotaa/JdocGitMojo.java
+++ src/main/java/pl/szotaa/JdocGitMojo.java
@@ -15,17 +15,36 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+/**
+ * Maven Plugin MOJO which generates JavaDocs and pushes them to GitHub Pages. Plugin assumes you
+ * enabled building GitHub Pages from /docs folder on master branch, your local git app is currently
+ * checked out at branch master and pushing won't prompt you for credentials.
+ * Created as a "hello world" Maven plugin.
+ *
+ * @author szotaa
+ */
+
 @Mojo(name = "JdocGit", defaultPhase = LifecyclePhase.DEPLOY)
 public class JdocGitMojo extends AbstractMojo {
+
+    /**
+     * Specifies which package should be javadoc'ed. Default value = 'com'.
+     */
 
     @Parameter(property = "subpackages", defaultValue = "com")
     private String subpackages;
 
-    @Parameter(property = "remoteName", defaultValue = "remote")
-    private String remoteName;
+    /**
+     * Specifies commit message which will be attached to java doc updating commit. Default = 'Update javadoc'
+     */
 
     @Parameter(property = "commitMessage", defaultValue = "Update javadoc")
     private String commitMessage;
+
+    /**
+     * Specifies time after which plugin assumes it failed. Default is 10, use larger values if you internet connection is slow
+     * or generated javadoc is huge.
+     */
 
     @Parameter(property = "timeout", defaultValue = "10")
     private String timeout;
@@ -42,12 +61,9 @@ public class JdocGitMojo extends AbstractMojo {
 
     }
 
-    private void pushToGithub() throws IOException, InterruptedException, MojoFailureException {
-        String projectRootPath = System.getProperty("user.dir");
-        handleProcess(projectRootPath, "git", "add", getPath("docs"));
-        handleProcess(projectRootPath, "git", "commit", "-m", commitMessage);
-        handleProcess(projectRootPath, "git", "push");
-    }
+    /**
+     * Generates Javadocs using following command: javadoc -d docs -sourcepath /pathToYourRepo/src/main/java -subpackages {subpackages variable}
+     */
 
     private void generateJavaDocs() throws IOException, InterruptedException, MojoFailureException {
         String projectRootPath = System.getProperty("user.dir");
@@ -56,6 +72,24 @@ public class JdocGitMojo extends AbstractMojo {
                 "-sourcepath", getPath("src", "main", "java"),
                 "-subpackages", subpackages);
     }
+
+    /**
+     * Git adds docs folder, commits with message specified in {commitMessage variable} and pushes to remote repository.
+     */
+
+    private void pushToGithub() throws IOException, InterruptedException, MojoFailureException {
+        String projectRootPath = System.getProperty("user.dir");
+        handleProcess(projectRootPath, "git", "add", getPath("docs"));
+        handleProcess(projectRootPath, "git", "commit", "-m", commitMessage);
+        handleProcess(projectRootPath, "git", "push");
+    }
+
+    /**
+     * Helper method which runs specified system commands, prints console output and handles timeouts.
+     *
+     * @param directory Directory of command execution.
+     * @param commands Array of commands and args.
+     */
 
     private void handleProcess(String directory, String... commands) throws IOException, InterruptedException, MojoFailureException {
         ProcessBuilder pb = new ProcessBuilder(commands);
@@ -68,12 +102,24 @@ public class JdocGitMojo extends AbstractMojo {
         }
     }
 
+    /**
+     * Helper method which gets absolute path to your project and appends subfolders specified in folders array.
+     * Uses correct file separators for multiple OSes.
+     *
+     * @param folders Subfolders appended to absolute project path.
+     * @return Correctly formatted file path.
+     */
+
     private String getPath(String... folders){
         String projectRootPath = System.getProperty("user.dir") + File.separator;
         StringJoiner stringJoiner = new StringJoiner(File.separator, projectRootPath, "");
         Arrays.stream(folders).forEach(stringJoiner::add);
         return stringJoiner.toString();
     }
+
+    /**
+     * Helper method which prints process response as Maven info.
+     */
 
     private void printResponse(InputStream stream){
         try{
@@ -86,6 +132,10 @@ public class JdocGitMojo extends AbstractMojo {
             getLog().info(e.getMessage());
         }
     }
+
+    /**
+     * Helper method converting timeout variable to an int.
+     */
 
     private int getTimeout(){
         return Integer.parseInt(timeout);
